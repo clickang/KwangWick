143d0b5ff3460dcf9228143887a6352768425824
Jang jiho <180173102+kaleido429@users.noreply.github.com>
2025-05-23 19:28:12 +0900

    타겟 폴더
    타겟 스크립트, 프리팹, 애니메이션

---
+++ Assets/Target/Target.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using System;
+
+public class Target : MonoBehaviour
+{
+    public event Action OnTargetDestroyed;
+
+    private bool isPeeking;
+
+    // 클래스 변수로 애니메이터 참조 추가
+    [SerializeField] private Animator animator;
+
+    public void Initialize(bool isPeeking)
+    {
+        this.isPeeking = isPeeking;
+
+        // 타겟 타입에 따른 초기화 로직
+        if (isPeeking)
+        {
+            SetupPeekingBehavior();
+        }
+        else
+        {
+            SetupMovingBehavior();
+        }
+    }
+
+    private void SetupPeekingBehavior()
+    {
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        // 랜덤하게 오른쪽(true) 또는 왼쪽(false)으로 설정
+        bool isRight = UnityEngine.Random.Range(0, 2) == 0;
+        animator.SetBool("right", isRight);
+
+        // 디버그 로그로 방향 확인
+        Debug.Log(isRight ? "Peeking to the right" : "Peeking to the left");
+    }
+
+    private void SetupMovingBehavior()
+    {
+        Debug.Log(transform.position.x);
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+        if (transform.position.x > -12f)
+        {
+            Debug.Log("left"); //x좌표 증가
+            animator.SetBool("right", false);
+        }
+        else
+        {
+            Debug.Log("right"); //x좌표 감소
+            animator.SetBool("right", true);
+        }
+    }
+
+    // 타겟이 파괴될 때 (히트 또는 시간 초과)
+    public void DestroyTarget()
+    {
+        OnTargetDestroyed?.Invoke();
+        Destroy(gameObject);
+    }
+
+    // 타겟이 맞았을 때 호출되는 메서드
+    public void Hit()
+    {
+        // 히트 효과, 점수 등의 로직
+        DestroyTarget();
+    }
+
+    /* Test
+    private void Test()
+    {
+        float randomDelay = UnityEngine.Random.Range(1f, 3f);
+        Invoke("DestroyTarget", randomDelay);
+    }
+    private void Start()
+    {
+        Test();
+    }
+    */
+}
